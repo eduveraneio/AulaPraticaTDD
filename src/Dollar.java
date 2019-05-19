8c47814629611a599dd40e0f6a7bcea4849987d2
Eduardo <Eduardo@192.168.15.9>
2019-05-19 15:26:49 -0300

    Passo1

---
+++ src/Dollar.java
@@ -0,0 +1,19 @@
+
+public class Dollar {
+
+	int amount;
+
+	Dollar(int amount) {
+		this.amount = amount;
+	}
+
+	void times(int multiplier) {
+		amount = amount * multiplier;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hellow World!");
+	}
+
+}
---
+++ src/DollarTest.java
@@ -0,0 +1,10 @@
+import junit.framework.TestCase;
+
+public class DollarTest extends TestCase {
+
+	public void testMultiplication() {
+		Dollar five = new Dollar(5);
+		five.times(2);
+		assertEquals(10, five.amount);
+	}
+}
